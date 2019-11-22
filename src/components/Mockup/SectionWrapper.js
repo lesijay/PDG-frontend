c416e179a7d94a109a0bb10eb22c07ef61e05377
lesijay <lesisampson@gmail.com>
2019-11-22 17:10:19 +0100

    create sectionwrapper component

---
+++ src/components/Mockup/SectionWrapper.js
@@ -0,0 +1,19 @@
+import React from "react";
+import  Sizing  from "./Sizing";
+
+const SectionWrapper = ({ children }) => {
+  return (
+    <Sizing width="100%" height="28rem" margin="8rem 0">
+      <Sizing
+        maxWidth="1440px"
+        display="flex"
+        alignItems="center"
+        margin="0 auto"
+      >
+        {children}
+      </Sizing>
+    </Sizing>
+  );
+};
+
+export default SectionWrapper;
